9216700e2fc736779d4e86235dab6784f0fa9c73
David Bootle <davidtbootle@gmail.com>
2021-03-22 09:48:03 -0400

    added automatic background updates

--- public/javascripts/socketutils.js
+++ public/javascripts/socketutils.js
@@ -1,6 +1,11 @@
 function addLogoffListener(socket) {
-
     socket.on("logoff", () => {
+        location.reload();
+    })
+}
+
+function addScoreboardListener(socket) {
+    socket.on('scoreboard-update', () => {
         location.reload();
     })
 }
