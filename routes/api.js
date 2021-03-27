653fedab39b80f3f3491fbc4e1700592123f7b84
David Bootle <davidtbootle@gmail.com>
2021-03-27 10:54:29 -0400

    Added final pieces of api

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,40 @@ var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 
+const requireAuth = async (req, res, next) => {
+    var authToken = req.headers.authorization;
+
+    if (authToken == null) {
+        res.status(401).send('Invalid authorization header.');
+        return;
+    }
+
+    var client = new MongoClient(req.app.get('databaseUrl'));
+
+    try {
+        await client.connect()
+
+        var teams = client.db('scoreboard').collection('teams');
+
+        var team = await teams.findOne({'token': authToken});
+
+        if (team == null) {
+            res.status(401).send('Invalid authorization header.');
+        } else {
+            req.team = team;
+
+            next();
+        }
+    }
+    catch (e) {
+        res.status(500).send('Database error');
+        console.dir(e);
+    }
+    finally {
+        client.close()
+    }
+}
+
 router.post('/get/teamscore', async (req, res) => {
 
     var id = req.body.id;
@@ -33,6 +67,98 @@ router.post('/get/teamscore', async (req, res) => {
     }
     finally {
         client.close();
+    }
+})
+
+router.post('/get/authtoken', async (req, res) => {
+
+    var id = req.body.id;
+    var password = req.body.password;
+
+    if (id == undefined || password == undefined) {
+        res.status(400).send('One or more required parameters are missing.')
+        return;
+    }
+
+    var client = new MongoClient(req.app.get('databaseUrl'));
+
+    try {
+        await client.connect()
+
+        var teams = client.db('scoreboard').collection('teams');
+
+        var team = await teams.findOne({id: id});
+        
+        if (team == null) {
+            res.status(404).send('No team exists with that id.');
+            return;
+        }
+
+        if (team.password != password) {
+            res.status(401).send('Invalid password for team with that id.');
+            return;
+        }
+
+        const generateAuthToken = req.app.get('generateAuthToken');
+        const authToken = generateAuthToken()
+
+        var result = await teams.updateOne({id: id}, { $set : {'token': authToken}});
+
+        if (result.modifiedCount == 0) {
+            res.status(500).send('Failed to update.');
+            return;
+        }
+
+        res.status(200).send(authToken);
+    }
+    catch (e) {
+        res.status(500).send('Database error');
+        console.dir(e);
+    }
+    finally {
+        client.close();
+    }
+})
+
+router.post('/set/teamscore', requireAuth, async (req, res) => {
+    var score = req.body.score;
+    var id = req.team.id;
+
+    if (score == undefined) {
+        res.status(400).send('One or more required parameters are missing.');
+        return;
+    }
+
+    if (parseInt(score) == NaN || score.length == 0 || score.length > 30 || !/^\-?[0-9]+$/.test(score)) {
+        res.status(400).send('One or more parameters failed validation.');
+        return;
+    }
+
+    var client = new MongoClient(req.app.get('databaseUrl'));
+
+    try {
+        await client.connect();
+
+        var teams = client.db('scoreboard').collection('teams');
+
+        var result = await teams.updateOne({'id': id}, { $set: {'score': score}});
+
+        if (result.modifiedCount == 0) {
+            res.status(500).send('Failed to update');
+            return;
+        } else {
+            res.status(200).send('ok');
+
+            var io = req.app.get('io');
+            io.emit('scoreboard-update');
+        }
+    }
+    catch (e) {
+        res.status(500).send('Database error');
+        console.dir(e);
+    }
+    finally {
+        client.close()
     }
 })
 
